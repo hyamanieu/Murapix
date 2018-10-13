e699fe9fce16e3b8c82bf0ead417e3d773669cca
Loued <loued2@gmail.com>
2018-10-13 11:53:24 +0200

    Import exemple flask program.

---
+++ app.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+from threading import Lock
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit, join_room, leave_room, \
+    close_room, rooms, disconnect
+
+# Set this variable to "threading", "eventlet" or "gevent" or None
+async_mode = None
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, async_mode=async_mode)
+thread = None
+thread_lock = Lock()
+
+
+def background_thread():
+    """Example of how to send server generated events to clients."""
+    count = 0
+    while True:
+        socketio.sleep(10)
+        count += 1
+        socketio.emit('my_response',
+                      {'data': 'Server generated event', 'count': count},
+                      namespace='/test')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', async_mode=socketio.async_mode)
+
+
+@socketio.on('my_event', namespace='/test')
+def test_message(message):
+    emit('my_response',
+         {'data': message['data']})
+
+
+@socketio.on('my_broadcast_event', namespace='/test')
+def test_broadcast_message(message):
+    emit('my_response',
+         {'data': message['data']},
+         broadcast=True)
+
+
+@socketio.on('join', namespace='/test')
+def join(message):
+    join_room(message['room'])
+    emit('my_response',
+         {'data': 'In rooms: ' + ', '.join(rooms())})
+
+
+@socketio.on('leave', namespace='/test')
+def leave(message):
+    leave_room(message['room'])
+    emit('my_response',
+         {'data': 'In rooms: ' + ', '.join(rooms())})
+
+
+@socketio.on('close_room', namespace='/test')
+def close(message):
+    emit('my_response', {'data': 'Room ' + message['room'] + ' is closing.'},
+         room=message['room'])
+    close_room(message['room'])
+
+
+@socketio.on('my_room_event', namespace='/test')
+def send_room_message(message):
+    emit('my_response',
+         {'data': message['data']},
+         room=message['room'])
+
+
+@socketio.on('disconnect_request', namespace='/test')
+def disconnect_request():
+    emit('my_response',
+         {'data': 'Disconnected!'})
+    disconnect()
+
+	
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    global thread
+    with thread_lock:
+        if thread is None:
+            thread = socketio.start_background_task(target=background_thread)
+    emit('my_response', {'data': 'Connected', 'count': 0})
+
+
+@socketio.on('disconnect', namespace='/test')
+def test_disconnect():
+    print('Client disconnected', request.sid)
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
